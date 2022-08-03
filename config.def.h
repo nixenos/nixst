dc640b0be0f9f09b87c55142d2738f57b535048d
Wojciech Janota <wojciech@janota.ovh>
2022-08-03 17:56:07 +0200

    Cattpuccin theme

--- config.def.h
+++ config.def.h
@@ -117,66 +117,41 @@ unsigned int tabspaces = 8;
 float alpha = 1.0;
 
 /* Terminal colors (16 first used in escape sequence) */
-// static const char *colorname[] = {
-//   "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-//   "#cc241d",
-//   "#98971a",
-//   "#d79921",
-//   "#458588",
-//   "#b16286",
-//   "#689d6a",
-//   "#a89984",
-//   "#928374",
-//   "#fb4934",
-//   "#b8bb26",
-//   "#fabd2f",
-//   "#83a598",
-//   "#d3869b",
-//   "#8ec07c",
-//   "#ebdbb2",
-//   [255] = 0,
-//   /* more colors can be added after 255 to use with DefaultXX */
-//   "#add8e6", /* 256 -> cursor */
-//   "#555555", /* 257 -> rev cursor*/
-//   "#282828", /* 258 -> bg */
-//   "#ffffff", /* 259 -> fg */
-// };
-
 static const char *colorname[] = {
-        "#1E1E28", /* hard contrast: #1d2021 / soft contrast: #32302f */
-        "#E28C8C",
-        "#B3E1A3",
-        "#EADDA0",
-        "#A4B9EF",
-        "#C6AAE8",
-        "#9DDDCB",
-        "#D7DAE0",
-        "#1E1E28", /* hard contrast: #1d2021 / soft contrast: #32302f */
-        "#E28C8C",
-        "#B3E1A3",
-        "#EADDA0",
-        "#A4B9EF",
-        "#C6AAE8",
-        "#9DDDCB",
-        "#6E6C7C",
-        [255] = 0,
-        /* more colors can be added after 255 to use with DefaultXX */
-        "#F7C196", /* 256 -> cursor */
-        "#ECBFBD", /* 257 -> rev cursor*/
-        "#1E1E28", /* 258 -> bg */
-        "#D7DAE0", /* 259 -> fg */
+	/* 8 normal colors */
+	"#45475A",
+	"#F38BA8",
+	"#A6E3A1",
+	"#F9E2AF",
+	"#89B4FA",
+	"#F5C2E7",
+	"#94E2D5",
+	"#BAC2DE",
+
+	/* 8 bright colors */
+	"#585B70",
+	"#F38BA8",
+	"#A6E3A1",
+	"#F9E2AF",
+	"#89B4FA",
+	"#F5C2E7",
+	"#94E2D5",
+	"#A6ADC8",
+
+[256] = "#CDD6F4", /* default foreground colour */
+[257] = "#1E1E2E", /* default background colour */
+[258] = "#F5E0DC", /*575268*/
+
 };
 
 
-
 /*
- * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 259;
-unsigned int defaultbg = 258;
-unsigned int defaultcs = 256;
-unsigned int defaultrcs = 257;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 258;
 
 /*
  * https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-Ps-SP-q.1D81
